27cc3a4fbe17ce884a864e269f6e65285a20c6ca
Sabitov <tsabitov@usgs.gov>
2019-02-11 11:12:56 -0500

    Updated main function. Added recursive calling for basin del and basin char. Added server name return to the log if returned values are different from the one in the clean room.

--- src/IntegrationWrapperV2.py
+++ src/IntegrationWrapperV2.py
@@ -1,5 +1,4 @@
 #Integration testing for watershed deliniation
-
 import traceback
 import datetime
 import time
@@ -8,20 +7,18 @@ import argparse
 import fnmatch
 import json
 import threading
-from WIMLib import WiMLogging
+from WIMLib.WiMLogging import WiMLogging
 from WIMLib import Shared
 from WIMLib.Config import Config
 from ServiceAgents.StreamStatsServiceAgent import StreamStatsServiceAgent
 from threading import Thread
-import threading
-import time
 import random
-from Queue import Queue
+import queue
 
 
 #Initial number of thread calls will be (N+1)
-simulThreads = 0           
-queue_list = Queue()
+simulThreads = 0
+queue_list = queue.Queue()
 
 
 #Create Summary.txt in the root folder of streamstats (Need to change directory)
@@ -38,14 +35,14 @@ workingDir = Shared.GetWorkspaceDirectory(config["workingdirectory"])
 #Used for command line
 parser = argparse.ArgumentParser()
 parser.add_argument("-file", help="specifies csv file location including gage lat/long and comid's to estimate", type=str, #Use the following LAT/LON pour point
-                    default = 'D:\ClientData\InputCoordinates.csv') #Change to the location of the csv file
+                    default = r'D:\ClientData\InputCoordinates.csv') #Change to the location of the csv file
 parser.add_argument("-inputEPSG_Code", help="Default WGS 84 (4326),see http://spatialreference.org/ref/epsg/ ", type=int,
                     default = '4326')
 args = parser.parse_args()
 
 
 #Check if file (Input.csv) is in csv
-if not os.path.isfile(args.file): raise Exception("File does not exist")
+if not os.path.isfile(args.file): raise Exception ("File does not exist")
 refDir = {"bdel":config["referenceFolderBasinDel"],"bchar":config["referenceFolderBasinChar"]}
 file = Shared.readCSVFile(args.file)
 headers = file[0]
@@ -58,22 +55,39 @@ file.pop(0)
 
 #Start logging txt
 startTime = time.time()
-WiMLogging.init(os.path.join(workingDir,"Temp"),"Integration.log")
+WiMLogging (os.path.join(workingDir,"Temp"),"Integration.log")
 
 
 #Count total number of rows, and initiate equal number of threads
 row_count = sum(1 for row in file) #Total number of sites
 maxThreads = row_count #Equal to the number of sites
 
+#timing decorator for functions
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print('{:s} function took {:.3f} s'.format(f.__name__, (time2-time1)))
+        return ret
+    return wrap
+  
+def findStr (x,y):
+    resultBChar = x
+    ind = [] #Look for string (value) inside of the parameters. We should have 20 of them returned from the server.
+    for i in range (0,len (resultBChar)):
+        tempind = str(resultBChar[i]).find(str(y))
+        ind.append (tempind)
+    return (sum(i > 0 for i in ind))
 
-#Main function that run by thread
+#Main function that run by a thread
 def run(i, q):
     rcode,x,y,refdir,iden,workspace = q.get() #Target function for threads to run to get sites
     try:
         run_func(rcode,x,y,refdir,iden,workspace) #main function for threads to run to get data from server
     except:
         tb = traceback.format_exc()
-        WiMLogging.sm("Error w/ run "+ tb)
+        WiMLogging().sm ("Error w/ run "+ tb)
     finally:
         q.task_done()
 
@@ -84,126 +98,79 @@ def run_func(rcode, x,y, path, siteIdentifier, workingDir):
     resultBChar = None
     resultBDel = None
 
-    def findStr (x,y):
-        resultBChar = x
-        ind = [] #Look for string (value) inside of the parameters. We should have 20 of them returned from the server.
-        c = 0
-        for i in range (0,len (resultBChar)):
-            tempind = str(resultBChar).find(str(y),c)
-            if tempind == -1: #When there none left, break the loop
-                break
-            c = tempind+1
-            ind.append (tempind)
-        return (ind)
 
     with StreamStatsServiceAgent() as sa: 
         try:
-            response = sa.getBasin(rcode,x,y,4326) #Get feature collection
-            k = 0
-            f1 = 0 #Flag 1  
-            f2 = 0 #Flag 2 
-            f3 = 0 #Flag 3
-
-
-            #Check the flag, if flag is active, attempt to get basin del one more time
-            #After 4 attempts, if response still none, it will break the loop since f2 = 4. 0 response result in print, failed to return from BDEL
-            while ((response == None or f1 == 1 or len(response)<1) and f2 < 4):
-                print 'New Attempt, while loop 1', siteIdentifier
-                response = sa.getBasin(rcode,x,y,4326) #Get feature collection
+            #Recursive call with 1 second interval for basin deliniation, limit to 5 calls
+            @timing
+            def rcBDel (f = 0):
                 try:
-                    len(response['featurecollection'][1]['feature']['features'][0]['geometry']['coordinates'][0]) #if coordinates exist, reset flag to 0
-                    f1 = 0
+                    if (f >4):
+                        return None
+                    else:
+                        response = sa.getBasin(rcode,x,y,4326)
+                        #attempting to create a new, fake var. If successful, continue, else it will rise an error.
+                        c = len(response[0]['featurecollection'][1]['feature']['features'][0]['geometry']['coordinates'][0])
+                        return (response) #Get feature collection
                 except:
-                    f2 = f2+1
+                    print ('Attempting another try for bDel')
+                    f +=1
+                    time.sleep (1)
+                    rcBDel (f)
 
-            
-            if (response !=None and f1 == 0): #if the flag to get.bdel set to 0 and there is a response
-                while (f3 == 0 and (response!= None or len(response)>0)):
-                    try:
-                        print 'Trying to get basin chars, there was a response for basin del', siteIdentifier
+            #Recursive call for basin characteristics, limit to 5 calls for basin characteristics and 5 calls for basin deliniation
+            @timing
+            def rcBChar ( rcode, response, j = 0):
+                try:
+                    if (j>4):
+                        return None
+                    else:
                         responseBChar = sa.getBChar (rcode,response['workspaceID']) #get basin characteristics from the server
-                        resultBChar = responseBChar['parameters'] #List of dictionaries
-                        resultBDel = response['featurecollection'][1]['feature']['features'][0]['geometry']['coordinates'][0] #List of lists
-                        HUCID = response ['featurecollection'][1]['feature']['features'][0]['properties']['HUCID']
-                        xy = [x,y]
-                        ind = findStr(resultBChar,'value') #count, how many values returned for basin characteristics for a unique (workspace id) ?
+                        X1 = findStr(list(responseBChar[0].values())[0], 'value')
+                        X2 = len (list(responseBChar[0].values())[0])
+                        if ( X1!=X2 ) : #if the number of returned values is not equal to the number of variables, rise an error
+                            raise ValueError
+                        return (responseBChar)
+                except:
+                    response = rcBDel (3)
+                    j+=1
+                    rcBChar (rcode, response[0], j)
+                    
 
-                        #If values returned for basin characteristics for that workspace id not satisfiying condition of equality, call function
-                        #for basin deliniation one more time, get workspace id and call function for basin characteristics. Store previous basin deliniation parameters.
-                        #If new basin deliniation parameters satisfying conditions and basin characteristics satisfying conditions, update basin deliniation and basin characteristics.
-                        #f3 is my flag for N number of calls
-
-
-                        if (len(ind)<len(resultBChar)): #Call basin characteristics if there any missing. If everything is ok, continue.
-                            print ('something is missing from basin characteristics')
-                            if (k == 0):
-                                responseBDel = resultBDel #save initial
-                                responseBCbar = resultBChar
-                            k=k+1
-                        else:
-                            f3 = 1
-
-                        if k == 4:
-                            resultBDel = responseBDel #if we reached the end, and nothing changed, reset to inial and continue
-                            resultBChar = responseBCbar
-                            f3=1
-
-
-                        if (k>0):
-                            responseNew = sa.getBasin(rcode,x,y,4326) #Recursive call
-                            #if response is null, reset to default one, else reset to new
-                            if (responseNew !=None):
-                                try:
-                                    len(responseNew['featurecollection'][1]['feature']['features'][0]['geometry']['coordinates'][0]) #if coordinates exist, reset flag to 0
-                                    f2 = 0
-                                except:
-                                    f2 = f2+1
-                            if (responseNew != None and f2 == 0): #if there is a response, and flag got tagged
-                                response = responseNew 
-                            else:
-                                print 'BDel return none'
-
-                            print 'New Attempt, While loop 2, Inner', siteIdentifier
-
-                    except:
-                        print 'inside of exception'
-                        resultBDel = response['featurecollection'][1]['feature']['features'][0]['geometry']['coordinates'][0]
-                        resultBChar = None
-                        f3 = 1
-
-                if (resultBChar == None):
-                    print "Finished: ", siteIdentifier
-                    raise Exception("{0} Failed to return from service Bchar".format(siteIdentifier))
-
-            else:
-                print 'failed to return from service BDEL'
-                print "Finished: ", siteIdentifier
-                raise Exception("{0} Failed to return from service BDel".format(siteIdentifier))   
+            #Get response of rcBDel
+            response = rcBDel ()
+            resultBDel = response[0]['featurecollection'][1]['feature']['features'][0]['geometry']['coordinates'][0] #List of lists
+            HUCID = response[0] ['featurecollection'][1]['feature']['features'][0]['properties']['HUCID']
+            xy = [x,y]
+            bdelServer = response[1]['Server']
+            resultBChar = rcBChar (rcode, response[0], 0)
+            bcharServer = resultBChar[1]['Server']
 
         except:
-            resultBDel = None
-            resultBChar = None
+            if (response == None):
+                resultBDel = None
+                resultBChar = None
 
         if resultBDel == None:
             fSummary = open('Summary.txt', 'a') 
             fSummary.write (str(siteIdentifier)+ ':' + ' Missing Return for BDel'+ '\n')
             fSummary.close ()
-            print "Finished: ", siteIdentifier
+            print ("Finished: ", siteIdentifier)
             raise Exception("{0} Failed to return from service BDel".format(siteIdentifier))
         else:
-            compare(resultBDel, path.get("bdel"),siteIdentifier,workingDir, HUCID, xy, rcode)
+            compare(resultBDel, path.get("bdel"),siteIdentifier,workingDir, HUCID, xy, rcode,bdelServer )
 
 
-        if (resultBChar == None or (len(ind)<len(resultBChar))):
+        if resultBChar == None:
             fSummary = open('Summary.txt', 'a') 
             fSummary.write (str(siteIdentifier)+ ':' + ' Missing Return for BChar'+ '\n')
             fSummary.close ()
-            print "Finished: ", siteIdentifier
+            print ("Finished with error: ", siteIdentifier)
             raise Exception ("{0} Failed to return from service Bchar".format(siteIdentifier))
         else:
-            compare(resultBChar, path.get("bchar"),siteIdentifier,workingDir, HUCID, xy, rcode)
+            compare(resultBChar, path.get("bchar"),siteIdentifier,workingDir, HUCID, xy, rcode, bcharServer)
 
-    print "Finished: ", siteIdentifier
+    print ("Finished: ", siteIdentifier)
 
 
 
@@ -214,49 +181,39 @@ def writeToJSONFile(path, fileName, data):  #Define function to write as json ob
             json.dump(data, fp)
     except:
         tb=traceback.format_exc()
-        WiMLogging.sm("Error writing json output "+tb)
+        WiMLogging().sm("Error writing json output "+tb)
 
 
-def compare(inputObj,path,ID, workingDir, HUCID, xy, rcode): #Compare json txt files
+def compare(inputObj,path,ID, workingDir, HUCID, xy, rcode, servName): #Compare json txt files
     try:  
         refObj = None
         refFile = os.path.join(path, ID+".json") #Get the reference json file from existing root folder
-        inputObj.sort()
-
-
-        if (type(inputObj[0])!=list): #Condition: If the inner object of the list is not a list then dictionary
+        
+        if (type(inputObj[0])==dict): #Condition: If the inner object of the list is not a list then dictionary
+            inputPars = inputObj[0]['parameters']
             i = 0
-            dictlist = [[] for _ in range (len(inputObj))] #Initialize list of lists
-
-            while i < len(inputObj):
-                dic = (inputObj[i]) #Extract dictionary object i
+            dictlist = [[] for _ in range (len(inputPars))] #Initialize list of lists
+            while i < len(inputPars):
+                dic = (inputPars[i]) #Extract dictionary object i
                 for key in sorted(dic): #Sort it by keys and extract each key, next, append to the dictionary
                     dictlist[i].append({key:str(dic[key])})
                 i += 1
             inputObj = dictlist #Return sorted list of lists instead of list of dictionaries for basin characteristics
     
-
         if os.path.isfile(refFile):
             with open (refFile) as f:
                 refObj = json.load(f)
             if inputObj!= refObj:
-                if (type(inputObj[0])!=list):
-                    dif = []
-                    for i in range (0, len(inputObj)):
-                        list_1 = inputObj[i]
-                        list_2 = refObj[i]
-                        pairs = zip(list_1, list_2)
-                        noteq = ([(x, y) for x, y in pairs if x != y])
-                        dif.append (noteq)
-                else:
-                    dif = []
-                    dif.append ([j for j in inputObj if not j in refObj])
+                dif = []
+                dif.append ([j for j in inputObj if not j in refObj])
+
                 if (path.find('Char')>0):
                     fSummary = open('Summary.txt', 'a') 
                     fSummary.write (str(ID)+ ':' + ' BChar not Equal'+ '\n')
                     fSummary.write (str(ID)+ ':' + str(HUCID) + ' HUCID'+ '\n')
                     fSummary.write (str(ID)+ ':' + str(xy) +' xy coordinates'+ '\n')
                     fSummary.write (str(ID)+ ':' + str(rcode) +' State'+ '\n')
+                    fSummary.write (str(ID)+ ':' + str(servName) +' Server '+ '\n') 
                     fSummary.write (str(ID)+ ':' + str(dif) +' Difference between NewCall and Ref'+ '\n')
                     fSummary.close ()
                 else:
@@ -265,9 +222,10 @@ def compare(inputObj,path,ID, workingDir, HUCID, xy, rcode): #Compare json txt f
                     fSummary.write (str(ID)+ ':' + str(HUCID) + ' HUCID'+ '\n')
                     fSummary.write (str(ID)+ ':' + str(xy) +' xy coordinates'+ '\n')
                     fSummary.write (str(ID)+ ':' + str(rcode) +' State'+ '\n')
+                    fSummary.write (str(ID)+ ':' + str(servName) +' Server '+ '\n')
                     fSummary.write (str(ID)+ ':' + str(dif) +' Difference between Newcall and Ref'+ '\n')
                     fSummary.close ()
-                WiMLogging.sm("Not equal Json's"+" "+ID)
+                WiMLogging().sm("Not equal Json's"+" "+ID)
                 writeToJSONFile(workingDir,ID+"_"+str(path.rsplit('/', 1)[-1]),inputObj) #Store in log folder
             else:
                 if (path.find('Char')>0):
@@ -279,7 +237,7 @@ def compare(inputObj,path,ID, workingDir, HUCID, xy, rcode): #Compare json txt f
                     fSummary.write (str(ID)+ ':' + 'BDel Equal Jsons' + '\n')
                     fSummary.close ()
                 tb = traceback.format_exc()
-                WiMLogging.sm("Equal Json's"+" "+ID+" "+ tb) #Don't create file
+                WiMLogging().sm("Equal Json's"+" "+ID+" "+ tb) #Don't create file
         else:
             if (path.find('Char')>0):#file not in reference folder, Create it
                 fSummary = open('Summary.txt', 'a') 
@@ -289,22 +247,12 @@ def compare(inputObj,path,ID, workingDir, HUCID, xy, rcode): #Compare json txt f
                 fSummary = open('Summary.txt', 'a') 
                 fSummary.write (str(ID)+ ':' + ' BDel New'+ '\n')
                 fSummary.close()
-            WiMLogging.sm("File not in reference folder"+" "+refFile)
+            WiMLogging().sm("File not in reference folder"+" "+refFile)
             writeToJSONFile(path, ID,inputObj)
     except:
         tb=traceback.format_exc()
-        WiMLogging.sm("Error Comparing "+tb)
+        WiMLogging().sm("Error Comparing "+tb)
         writeToJSONFile(workingDir, ID+"_viaError",{'error':tb})
-
-'''def getBasin2 (x,y,region,ID, f=0): #Recursive calling 
-    try:
-        if (f>4):
-            return None
-        else:
-            #Do smth
-    except:
-        getBasin2 (x,y,region,ID, f+=1)'''
-
 
 #Main thread
 for i in range(maxThreads): #Run threads as daemon, so they close when finish
@@ -313,19 +261,18 @@ for i in range(maxThreads): #Run threads as daemon, so they close when finish
     time.sleep(0.1)
     worker.start()
 
-
-
 #Global var counting number of active threads
 threadsINI = threading.active_count() 
 f = 0
+
 for row in file: #Query to invoke threads !
-    print 'Calling Input: ', row[uniqueID]
     queue_list.put((row[rcode],row[x],row[y],refDir,row[uniqueID], workingDir))
-    WiMLogging.sm('***Calling Input: '+ str(f))
+    WiMLogging().sm('***Calling Input: '+ row[uniqueID])
+
     if f == simulThreads:
         while f == simulThreads and threading.active_count() == threadsINI: #Listener
             pass            #Inifite loop waiting for thread to be done with work
-            time.sleep (1) #There is a chance to run into error if two threads finished simultaniously within 0.1 or higher interval
+            time.sleep (0.5) #There is a chance to run into error if two threads finished simultaniously within 0.1 or higher interval
         if threading.active_count()<threadsINI:
             print ('Initialized') #Each initialized statement should follow Finished one
             f = simulThreads - (threadsINI-threading.active_count ())+1
@@ -335,4 +282,4 @@ for row in file: #Query to invoke threads !
 
 queue_list.join() #Close mainthread after child threads done working
 
-print '*** Done'
+print ('*** Done')
